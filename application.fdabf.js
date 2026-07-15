3f2f4c94a035ce046ddacf90ccabc6b58a765e0f
yyc2644 <yichengace@hotmail.com>
2026-07-15 10:55:17 +0800

    deploy fingerprinted crash fix

--- application.fdabf.js
+++ application.fdabf.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.json';
+          this.settingsPath = 'src/settings.2dc08.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.017f9.js
+++ index.017f9.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.fdabf.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
